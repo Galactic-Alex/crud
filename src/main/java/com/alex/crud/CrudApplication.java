f3395bbbdfa95e1fa01d29fbeca33754012ebb49
Galactic Alex <Lehahahaigaleksus@mail.ru>
2024-03-27 14:04:37 +0300

    application.yml changes

--- src/main/java/com/alex/crud/CrudApplication.java
+++ src/main/java/com/alex/crud/CrudApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CrudApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(Long.MAX_VALUE);
         SpringApplication.run(CrudApplication.class, args);
     }
 
